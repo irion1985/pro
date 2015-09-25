4b309ce412aafed9b2f77311fbc46efc41780974
Your Name <you@example.com>
2015-09-25 11:06:38 +0900

    prohealth reload am11:06

---
+++ src/main/java/com/webapp/model/MemberModel.java
@@ -0,0 +1,6 @@
+package com.webapp.model;
+
+public class MemberModel {
+	String str = "집에간다";
+	String str1 = "다시옴";
+}
---
+++ src/main/java/com/webapp/service/MemberService.java
@@ -0,0 +1,5 @@
+package com.webapp.service;
+
+public class MemberService {
+	String str1 = "오늘 먼저감";
+}
